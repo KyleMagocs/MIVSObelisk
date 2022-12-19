b051b5425b4785a29b5689af0c1b38afb0a73afb
Kyle Magocs <kylemagocs+github@gmail.com>
2022-12-19 12:41:23 -0500

    Update palette.h
    adding HANG ON FAT BOY palette

--- include/palette.h
+++ include/palette.h
@@ -133,6 +133,24 @@ DEFINE_GRADIENT_PALETTE( yarg_gp ) {
   255, 255,255,255};
 
 
+// Gradient palette "hangonfatboy_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rc/tn/hangonfatboy.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( hangonfatboy_gp ) {
+    0, 215, 74,  6,
+   30,  74, 22, 53,
+   61,  18,  6, 27,
+   96,   5, 36, 85,
+  114,   1,  8, 30,
+  140,   1,  8, 30,
+  158,   5, 36, 85,
+  193,  18,  6, 27,
+  224,  74, 22, 53,
+  255, 215, 74,  6};
+
+
 CRGBPalette16 allPalettes[] = {
     Dark2_06_gp,
     bhw2_50_gp,
@@ -140,6 +158,7 @@ CRGBPalette16 allPalettes[] = {
     ramp_gp,
     cool_gp,
     yarg_gp,
+    hangonfatboy_gp,
     // WhiteStripes_p // ack it just looks like ass because I'm not injecting better power
     winter_gp
 };
