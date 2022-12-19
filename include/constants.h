e7f031c6507898c7865415203d89dee1307513a3
Kyle Magocs <kylemagocs+github@gmail.com>
2022-12-19 10:39:00 -0500

    swapping out the OLED library; much faster now

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,7 @@
 #ifndef Constants_h
 #define Constants_h
 
-char VERSIONSTRING[] =  "OBELISK v0.2a";
+char VERSIONSTRING[] =  "OBELISK v0.3a";
 
 #define PATTERN_SWAP 60*10
 #define UPDATES_PER_SECOND 30
@@ -16,7 +16,7 @@ const int NUM_LEDS = LEDS_PER_STRIP * NUM_CUBES;
 
 #define BRIGHTNESS 128
 
-#define OLED_DRAW 5
+#define OLED_DRAW 1
 
 #define LED_PIN_1 17
 #define LED_PIN_2 18
